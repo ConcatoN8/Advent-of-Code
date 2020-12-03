559e7951eee7ba5691d2f5130a7e922b246977a6
ConcatoN8 <75445661+ConcatoN8@users.noreply.github.com>
2020-12-03 10:33:02 -0800

    Update a2b.pl

--- 2020/a2b.pl
+++ 2020/a2b.pl
@@ -1,57 +1,53 @@
 #!/usr/bin/perl
 
 use warnings;
-#use diagnostics;
+use diagnostics;
 
 $count = 0;
 while(<>)
 {
     chomp();
-#    print "$_\n";
-if ($_ =~ /(.*)-(.*) (.*): (.*)/gi)
-{
+    if ($_ =~ /(.*)-(.*) (.*): (.*)/gi)
+    {
+        $lower = $1;
+        $upper = $2;
+        $letter = $3;#letter
+        $code = $4;#code
 
-    $lower = $1;
-    $upper = $2;
-    $letter = $3;#letter
-    $code = $4;#code
+        $charCount = 0;    
+        foreach $char (split//, $code)
+        {
 
-$charCount = 0;    
-foreach $char (split//, $code)
-{
-
-if($char =~ $letter)
-{
-print "$char\n";
-    $charCount++;
-}#if Char
+            if($char =~ $letter)
+            {
+                print "$char\n";
+                $charCount++;
+            }#if Char
 
 
-}#forEach
+        }#forEach
 
-print "Upper: $upper\n";
-print "Lower: $lower\n";
+    #TEST PARAMS
+    #print "Upper: $upper\n";
+    #print "Lower: $lower\n";
 
-if(substr($code, $lower - 1 , 1) =~ $letter && substr($code, $upper - 1, 1) =~ $letter)
-{
-    print "Both\n";
-}#if substr Both
+    if(substr($code, $lower - 1 , 1) =~ $letter && substr($code, $upper - 1, 1) =~ $letter)
+    {
+        print "Both\n";#TEST PARAM
+    }#if substr Both
 
-elsif(substr($code, $lower - 1, 1) =~ $letter && substr($code, $upper - 1, 1) !~ $letter)
-{
+    elsif(substr($code, $lower - 1, 1) =~ $letter && substr($code, $upper - 1, 1) !~ $letter)
+    {
+        $count++;
+        print "Got one\n";#TEST PARAM
+    }#if substr FIRST
+    elsif(substr($code, $lower - 1, 1) !~ $letter && substr($code, $upper - 1, 1) =~ $letter)
+    {
     $count++;
-    print "Got one\n";
-}#if substr FIRST
-elsif(substr($code, $lower - 1, 1) !~ $letter && substr($code, $upper - 1, 1) =~ $letter)
-{
-    $count++;
-    print "Got one\n";
-}#if substr SECOND   
+    print "Got one\n";#TEST PARAM
+    }#if substr SECOND   
 
 
-}
-#if
-
-} 
-#while<>
+    }#if
+}#while<>
 print "$count\n";
